7bdf961bbb22cd59978c3a0c11f5bd384fb56996
Wiki4831 <96461229+Wiki4831@users.noreply.github.com>
2022-01-20 21:57:17 +0800

    Add files via upload

--- web/src/pages/RegisterComponents/Review.js
+++ web/src/pages/RegisterComponents/Review.js
@@ -2,10 +2,10 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import moment from 'moment'
+import {placeid} from '../RegBeenPlace'
+import {phonenum} from '../RegBeenPlace'
 
 const time = Date.now();
-const PhoneNum='0912345678';
-const PlaceID='03807654'
 export default function Review() {    
   return (
     <React.Fragment>
@@ -18,9 +18,9 @@ export default function Review() {
             疫調登記
           </Typography>
           <Typography gutterBottom>場所代碼 </Typography>
-          <Typography gutterBottom>{PlaceID}</Typography>
+          <Typography gutterBottom>{placeid}</Typography>
           <Typography gutterBottom>手機號碼 </Typography>
-          <Typography gutterBottom>{PhoneNum}</Typography>
+          <Typography gutterBottom>{phonenum}</Typography>
           <Typography gutterBottom>登記時間 </Typography>
           <Typography gutterBottom>{moment(time).format('YYYY-MM-DDTHH:mm:ss')}</Typography>
         </Grid>
